59504282ea575c51f910fa35a915d1991964a5c6
vargasjuanibarra <78344342+vargasjuanibarra@users.noreply.github.com>
2021-03-16 14:01:21 +0800

    added UI in ejs

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter = require('./routes/index')
-const bebot = require('./routes/sample')
 
+app.use(expressLayouts)
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+
+
+
+// MONGO DATABASE CONNECTION
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
@@ -22,7 +23,15 @@ const db = mongoose.connection
 db.on('error', error => console.error('error'))
 db.once('open', () => console.log('Connected to mongoose'))
 
-app.use('/', indexRouter) 
-app.use('/bebot', bebot)
+// ROUTES
 
-app.listen(process.env.PORT || 3000)
+const indexRouter = require('./routes/index')
+const users = require('./routes/users')
+
+app.use('/', indexRouter) 
+app.use('/users', users)
+
+// SERVER PORT
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`))    
